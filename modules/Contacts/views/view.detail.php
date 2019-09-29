baa41ba7a5a9922ee037aa5211a7a59ad09c6106
Ubuntu <ubuntu@ip-172-31-44-240.ec2.internal>
2019-09-29 02:31:33 +0000

    Se quita mensaje de prueba en View Detail de Contactos

--- modules/Contacts/views/view.detail.php
+++ modules/Contacts/views/view.detail.php
@@ -5,7 +5,6 @@ require_once 'modules/Contacts/views/view.detail.php';
 class CustomContactsViewDetail extends ContactsViewDetail{
 
     function display(){
-        echo "Daniel2!";
         parent::display();
     }
 }
